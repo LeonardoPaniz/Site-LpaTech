fa5b0eae614cfaa17655cf602e07a22de5f090e8
leonardopaniz <leonardopaniz@outlook.com>
2023-11-09 07:43:07 -0300

    j

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { LoadingComponent } from './components/loading/loading.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoadingService } from './service/loading.service';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { FooterComponent } from './components/footer/footer.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     LoadingComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule
+    RouterModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [LoadingService],
   bootstrap: [AppComponent]
